6332241e19e308bc4d983483262f4403bb118d5d
Chris Raschl <cr@kautsig.org>
2014-06-15 11:36:04 +0200

    Add more formatting options

--- org-weather.el
+++ org-weather.el
@@ -37,7 +37,18 @@
 
 ;; Set this to your location
 (defvar org-weather-location "Graz,AT")
-(defvar org-weather-unit "°C")
+
+;; Define the format string to display in the agenda, see below for available wildcards
+(defvar org-weather-format "Weather: %desc, %tmin-%tmax%tu, %p%pu, %h%hu, %s%su")
+
+;; The api url, no need to change ususally
+(defvar org-weather-api-url "http://api.openweathermap.org/data/2.5/forecast/daily?q=%s&mode=json&units=metric&cnt=7")
+
+;; The units, just for displaying
+(defvar org-weather-temperature-unit "°C")
+(defvar org-weather-pressure-unit "hpa")
+(defvar org-weather-humidity-unit "%")
+(defvar org-weather-speed-unit "m/s")
 
 ;; Variables for internal use only
 (defvar org-weather-initialized nil)
@@ -48,31 +59,69 @@
   "Loads the webserivce data and returns it as a JSON object."
   (with-temp-buffer
     (url-insert-file-contents
-     (format "http://api.openweathermap.org/data/2.5/forecast/daily?q=%s&mode=json&units=metric&cnt=7" org-weather-location))
+     (format org-weather-api-url org-weather-location))
     (json-read)))
 
 (defun org-weather-add-to-cache (item)
   "Adds the one result 'list' item to the data cache"
   (let* ((timestr (cdr (assoc 'dt item)))
          (cache-key (format-time-string "%F" (seconds-to-time timestr)))
+
+         ;; extract fields from the 'weather' element
          (weather (cdr (assoc 'weather item)))
+         (main (org-weather-weather weather 'main))
+         (desc (org-weather-weather weather 'description))
+         (icon (org-weather-weather weather 'icon))
+
+         ;; extract fields from the 'temp' element
          (temperature (cdr (assoc 'temp item)))
-         (temp-min (number-to-string (org-weather-temp-min temperature)))
-         (temp-max (number-to-string (org-weather-temp-max temperature)))
-         (data (concat (org-weather-description weather) ", " temp-min org-weather-unit " - " temp-max org-weather-unit)))
-    (puthash cache-key data  org-weather-data)))
+         (tmin (number-to-string (round (org-weather-temp temperature 'min))))
+         (tmax (number-to-string (round (org-weather-temp temperature 'max))))
+         (tmorn (number-to-string (round (org-weather-temp temperature 'morn))))
+         (tday (number-to-string (round (org-weather-temp temperature 'day))))
+         (teve (number-to-string (round (org-weather-temp temperature 'eve))))
+         (tnight (number-to-string (round (org-weather-temp temperature 'night))))
 
-(defun org-weather-description (weather)
-  "Extracts the description from a 'weather' element"
-  (cdr (assoc 'description (elt weather 0))))
+         ;; other fields
+         (pressure (number-to-string (round (cdr (assoc 'pressure item)))))
+         (humidity (number-to-string (round (cdr (assoc 'humidity item)))))
+         (speed (number-to-string (round (cdr (assoc 'speed item)))))
 
-(defun org-weather-temp-min (temperature)
-  "Extracts the min element from a 'temp' element"
-  (cdr (assoc 'min temperature)))
+         ;; replace in org-weather-format
+         (agenda-str org-weather-format)
 
-(defun org-weather-temp-max (temperature)
-  "Extracts the max element from a 'temp' element"
-  (cdr (assoc 'max temperature)))
+         (agenda-str (org-weather-replace "%main" main agenda-str))
+         (agenda-str (org-weather-replace "%desc" main agenda-str))
+         (agenda-str (org-weather-replace "%icon" main agenda-str))
+
+         (agenda-str (org-weather-replace "%tu" org-weather-temperature-unit agenda-str))
+         (agenda-str (org-weather-replace "%tmin" tmin agenda-str))
+         (agenda-str (org-weather-replace "%tmax" tmax agenda-str))
+         (agenda-str (org-weather-replace "%tmorn" tmorn agenda-str))
+         (agenda-str (org-weather-replace "%tday" tday agenda-str))
+         (agenda-str (org-weather-replace "%teve" teve agenda-str))
+         (agenda-str (org-weather-replace "%tnight" tnight agenda-str))
+
+         (agenda-str (org-weather-replace "%pu" org-weather-pressure-unit agenda-str))
+         (agenda-str (org-weather-replace "%p" pressure agenda-str))
+         (agenda-str (org-weather-replace "%hu" org-weather-humidity-unit agenda-str))
+         (agenda-str (org-weather-replace "%h" humidity agenda-str))
+         (agenda-str (org-weather-replace "%su" org-weather-speed-unit agenda-str))
+         (agenda-str (org-weather-replace "%s" speed agenda-str))
+
+         )
+    (puthash cache-key agenda-str org-weather-data)))
+
+(defun org-weather-replace (what with in)
+  (replace-regexp-in-string (regexp-quote what) with in))
+
+(defun org-weather-weather (weather field)
+  "Extracts a given field from the first element in the 'weather' list"
+  (cdr (assoc field (elt weather 0))))
+
+(defun org-weather-temp (temperature field)
+  "Extracts a given field element from a 'temp' element"
+  (cdr (assoc field temperature)))
 
 (defun org-weather-refresh ()
   "Refreshes the weather data"
@@ -93,8 +142,7 @@
       (org-weather-refresh)
       (setq org-weather-initialized t))
     (let ((org-weather-raw (gethash cache-key org-weather-data)))
-      (when org-weather-raw
-        (format "Weather: %s" org-weather-raw)))))
+      (when org-weather-raw org-weather-raw))))
 
 (defun org-weather ()
   "Usable as sexp expression in the diary or an org file."
